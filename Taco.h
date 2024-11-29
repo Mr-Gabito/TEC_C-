b90de1a499b6bcdbd56edee032ca1847d1793e7b
Mr-Gabito <a01709333@tec.mx>
2024-11-29 07:26:52 -0600

    Update Taco.h

--- Taco.h
+++ Taco.h
@@ -1,7 +1,42 @@
 #include <iostream>
+#ifndef TACO_H
+#define TACO_H
 using namespace std;
 
-int main() {
-    cout << "Hello World!";
-    return 0;
-}
+class Taco {
+    private:
+    string meat;
+    float price;
+    const string meats[3] = {"Pastor", "Chorizo", "Chuleta"};
+    const float prices[3] = {20.99, 15.99, 28.99};
+
+    public: 
+
+    //default constructor
+    Taco(): meat("Pastor"), price(prices[0]) {}
+
+
+    Taco(string chmeat) {
+        bool valid_meat = false;
+
+        for (int i = 0; i < 3; ++i) {
+            if (chmeat == meats[i]) {
+                meat = chmeat;
+                price = prices[i];
+                valid_meat = true;
+                break;
+            }
+        }
+        if (!valid_meat) {
+            cout << "That is not a meat in our menu, I think you meant Pastor! " << endl;
+            meat = "Pastor";
+            price = prices[0];
+        }
+    }
+
+    string get_meat() {return meat;};
+    float get_price() {return price;};
+
+};
+
+#endif
